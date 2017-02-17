adafde47f2558a2c464ad8ff6a511814a07b8e4d
FLofox <FLofox@users.noreply.github.com>
2017-02-17 10:29:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,20 +6,12 @@ int main(){
 
 // ICi mettre le compteur, attention si fichier d'entrée, le charger sur le dernier ID
 
-struct Annonce
-
-{
-    unsigned int id;                    //Numéro d'annonce
-    unsigned int type = 0;              //Code de type (0 = appart, 1 = maison)
-    unsigned int pieces= 1;             //Nombre de pièces       
-    unsigned float surface = 9;             
-    unsigned float loyer = 0;
-    unsigned float charges = 0;
-    unsigned float prix = 0;            //Loyer + Charges
-
-};
-
-typedef struct Annonce Annonce;        
 
 Liste liste = NULL;
 
+    
+    
+    
+
+    return 0;    
+} 
