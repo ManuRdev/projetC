31745e40af891d49caecfed3601c717160a520ab
FLofox <FLofox@users.noreply.github.com>
2017-02-17 10:24:12 +0100

    Create annonces.h

---
+++ annonces.h
@@ -0,0 +1,14 @@
+#ifndef ANNNONCES_H_INCLUDED
+#define ANNONCES_H_INCLUDED
+
+
+// Definition de la structure d'une annonce
+
+// On défini l'alais
+typedef Annonce* Liste;
+
+Liste creerAnnonce(Liste liste, int id);
+
+Liste afficher(Liste liste);
+
+#endif 
