0a51ac5933c47644c760fedd1a7426056b4388f1
FLofox <FLofox@users.noreply.github.com>
2017-02-17 11:06:29 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,8 +10,54 @@ int main(){
 
 Liste liste = NULL;
 
-    
-    
+// MENU d'ACCUEIL
+    int choix_accueil =0;
+    printf("Bienvenue à l'Enseigne Spécialisée des Solution Immobilières !\nQue voulez vous faire ?\n 1 : Ajouter une annonce\n2 : Afficher les annonces\n3 : Trier les annonces par type (appartement ou maison)\n4 : Chercher par nombre de pièces\n5 : Chercher par surface mini\n6 : Recherche multi-critères\n7 : Tri et recherche par loyer\n8 : Exporter les annonces\n9 : Charger les annonces\n10 : Quitter\n\n");
+    scanf(%d", &choix_accueil;
+    switch(choix_accueil)
+          {
+              case :1 :
+                    // Instructions
+                    break;
+              
+              case :2 :
+                    // Instructions
+                    break;
+              
+              case :3 :
+                    // Instructions
+                    break;
+              
+              case :4 :
+                    // Instructions
+                    break;              
+              
+              case :5 :
+                    // Instructions
+                    break;
+              
+              case :6 :
+                    // Instructions
+                    break;
+              
+              case :7 :
+                    // Instructions
+                    break;
+              
+              case :8 :
+                    // Instructions
+                    break;
+              
+              case :9 :
+                    // Instructions
+                    break;              
+              
+              case :10 :
+                    // Instructions
+                    break;
+          }
+              
+              
     
 
     return 0;    
