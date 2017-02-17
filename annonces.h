be4ff17557123cea255c573938702cf549e41d26
FLofox <FLofox@users.noreply.github.com>
2017-02-17 10:25:48 +0100

    Update annonces.h

--- annonces.h
+++ annonces.h
@@ -3,10 +3,21 @@
 
 
 // Definition de la structure d'une annonce
+struct Annonce
+{
+    unsigned int id;                    //Numéro d'annonce
+    unsigned int type = 0;              //Code de type (0 = appart, 1 = maison)
+    unsigned int pieces= 1;             //Nombre de pièces       
+    unsigned float surface = 9;             
+    unsigned float loyer = 0;
+    unsigned float charges = 0;
+    unsigned float prix = 0;            //Loyer + Charges
+}Annone; 
 
 // On défini l'alais
 typedef Annonce* Liste;
 
+//Fonctions
 Liste creerAnnonce(Liste liste, int id);
 
 Liste afficher(Liste liste);
